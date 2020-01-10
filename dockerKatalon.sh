588e6040b52dd14cec72587b9622f397709b3baa
flareblare <49785717+flareblare@users.noreply.github.com>
2020-01-10 15:37:34 +0100

    Update dockerKatalon.sh
    changed version from latest to 6.3.3

--- dockerKatalon.sh
+++ dockerKatalon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/chrome:/katalon/katalon/report katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=e08517c1-f1c7-4292-9494-ae4931cedcdd
-sleep 30
+sleep 33
 docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/firefox:/katalon/katalon/report katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=e08517c1-f1c7-4292-9494-ae4931cedcdd
