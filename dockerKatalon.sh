efbaa35782e4b603f9e45800950a1bfc3d0b6cba
flareblare <49785717+flareblare@users.noreply.github.com>
2020-01-10 15:40:18 +0100

    Update dockerKatalon.sh
    New api key

--- dockerKatalon.sh
+++ dockerKatalon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/chrome:/katalon/katalon/report katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=e08517c1-f1c7-4292-9494-ae4931cedcdd
+docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/chrome:/katalon/katalon/report katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=3cc4c4ee-18da-4a5e-9c31-8e00312eb18d
 sleep 33
-docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/firefox:/katalon/katalon/report katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=e08517c1-f1c7-4292-9494-ae4931cedcdd
+docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/firefox:/katalon/katalon/report katalonstudio/katalon:6.3.3 katalon-execute.sh -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=3cc4c4ee-18da-4a5e-9c31-8e00312eb18d
+
