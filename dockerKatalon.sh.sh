afc8b70f097cd6a273bcfd4ceba162e4188cd333
flareblare <49785717+flareblare@users.noreply.github.com>
2020-01-10 15:36:47 +0100

    Delete dockerKatalon.sh.sh
    duplicate

--- dockerKatalon.sh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/chrome:/katalon/katalon/report katalonstudio/katalon katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=e08517c1-f1c7-4292-9494-ae4931cedcdd
-sleep 30
-docker run -t --rm -v "$(pwd)":/katalon/katalon/source   -v $(pwd)/reports/chrome:/katalon/katalon/report katalonstudio/katalon katalon-execute.sh -browserType="Firefox" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TestEveryIBE" -apiKey=e08517c1-f1c7-4292-9494-ae4931cedcdd
